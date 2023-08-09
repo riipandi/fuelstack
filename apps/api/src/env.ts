e1eaea860fbbdd97622e186ad5a8d38d63fcc432
Aris Ripandi <riipandi@gmail.com>
2023-08-09 23:21:41 +0700

    add garage metric security access

--- apps/api/src/env.ts
+++ apps/api/src/env.ts
@@ -21,5 +21,14 @@ export const env = zennv({
     PORT: z.number().default(5100),
     HOST: z.string().default('0.0.0.0'),
     DATABASE_URL: z.string(),
+    // S3 storage configuration
+    // CloudFlare R2 endpoint: https://xxxxxxxxx.r2.cloudflarestorage.com
+    // Garage S3 endpoint: http://localhost:3900
+    S3_CDN_URL: z.string().default('http://localhost:3902'),
+    S3_ENDPOINT: z.string().optional(),
+    S3_ACCESS_KEY_ID: z.string().optional(),
+    S3_ACCESS_KEY_SECRET: z.string().optional(),
+    S3_BUCKET_NAME: z.string().default('fuelstack'),
+    S3_REGION: z.string().default('auto'),
   }),
 })
