9e280ef93c23e577f0cfba413ce7773d7513f86e
Aris Ripandi <riipandi@gmail.com>
2023-08-11 01:04:27 +0700

    optimize tsup configuration

--- packages/database/tsup.config.ts
+++ packages/database/tsup.config.ts
@@ -1,5 +1,18 @@
 import { defineConfig, Options } from 'tsup'
 
 export default defineConfig((options: Options) => ({
+  outDir: 'dist',
+  entry: {
+    index: 'src/index.ts',
+  },
+  format: ['cjs', 'esm'],
+  silent: true,
+  splitting: false,
+  sourcemap: true,
+  minify: !options.watch,
+  clean: true,
+  dts: true,
+  target: 'es2020',
+  injectStyle: false,
   ...options,
 }))
--- packages/ui-library/tsup.config.ts
+++ packages/ui-library/tsup.config.ts
@@ -1,6 +1,20 @@
 import { defineConfig, Options } from 'tsup'
 
 export default defineConfig((options: Options) => ({
+  outDir: 'dist',
+  entry: {
+    index: 'src/index.tsx',
+  },
+  format: ['cjs', 'esm'],
+  silent: true,
+  splitting: false,
+  sourcemap: true,
+  minify: !options.watch,
+  clean: true,
+  dts: true,
+  target: 'es2020',
+  injectStyle: false,
+  external: ['react'],
   banner: {
     js: "'use client'",
   },
